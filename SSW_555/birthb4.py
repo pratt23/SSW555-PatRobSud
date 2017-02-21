d0cbbd174b98102d0bfe003b6478095dacbf488a
basilmajdi <bmajdi@stevens.edu>
2017-02-21 18:14:47 -0500

    Birth Before Death function
    Needs a bit of attention in testing it

---
+++ SSW_555/birthb4.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 21:46:16 2017
+
+@author: basilmajdi
+"""
+
+from datetime import datetime
+import unittest
+        
+def convert_2date(date1):
+    return datetime.strptime(date1, '%d %b %Y')    
+
+def birth_b4_dth(dob, dod):
+    
+
+    
+    if dod == "None": 
+        return True
+    
+    elif convert_2date(dod) < convert_2date(dob):    
+        return False
+    else:    
+        return True
+
+
+
+
+class Test_my_functions(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(birth_b4_dth('3 MAR 2015', '3 MAR 2015'))
+        self.assertTrue(birth_b4_dth('26 APR 2014', '3 MAR 2015'))
+        
+    
+    def test_wrong(self):
+        self.assertFalse(birth_b4_dth('3 MAR 2017', '3 MAR 2015'))
+        self.assertFalse(birth_b4_dth('3 MAR 2017', '24 DEC 2011'))
+        
+    
+        
+if __name__=='__main__':
+    unittest.main(exit=False)
