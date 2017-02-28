1e7dd37f2a1492873b201489b8f324102bbc89f4
Prateek <pratt23@gmail.com>
2017-02-27 20:48:44 -0500

    Update prateek.py

--- SSW_555/prateek.py
+++ SSW_555/prateek.py
@@ -1,6 +1,14 @@
-#!/usr/bin/python
+from datetime import datetime
+from datetime import date
 
 def sibling_nos(sibs):
 	if len(sibs)>16:
 		sibs.append('MoreThan16')
 	return len(sibs)
+
+def convert_2date(dob): 
+    return datetime.strptime(dob, '%d %b %Y') 
+
+def calculate_age(dob):
+    today = date.today()
+    return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
